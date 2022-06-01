34339b504c142ec7b5701084bb8f6c181f827019
Jorge Sánchez Aymar <90512447+JorgeAymar@users.noreply.github.com>
2022-06-01 11:03:34 -0400

    Add files via upload

---
+++ Taller1/js/miarchivo.js
@@ -0,0 +1,9 @@
+let nombre = prompt("Ingresa tu nombre");
+let n = parseInt(prompt("ingresa un valor"));
+
+
+for (let i=0; i <= n; i++){
+    console.log(i + ')Tu nombre es: ' + nombre)
+}
+
+console.log('Fin del programa')
